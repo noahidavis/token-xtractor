ebcbf779bf34fa340ff50572c3bdc0d91401d86d
Bernie Chen <96664333+bcscc@users.noreply.github.com>
2024-11-20 18:25:22 -0500

    Update Support & About copy

--- src/app/components/HelpModalTabs.tsx
+++ src/app/components/HelpModalTabs.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Tabs, Box, Text, Flex, Code, ScrollArea } from '@radix-ui/themes';
+import { Tabs, Box, Text, Flex, Code, ScrollArea, Link, Strong} from '@radix-ui/themes';
 
 const HelpModalTabs: React.FC = () => {
     
@@ -69,11 +69,51 @@ const HelpModalTabs: React.FC = () => {
                     </Tabs.Content>
 
                     <Tabs.Content value="support">
-                        <Text size="2">Get help, give help.</Text>
+                        <Flex direction='column' gap='3'>
+                            <Text className='options-label' size='3'>Get help, Give help!</Text>
+                            <Flex direction='column' gap='2'>
+                                <Text size='2'>
+                                    If you have questions, need guidance, want to report a bug, or have feature requests, please don't hesitate to reach out.
+                                </Text>
+                            </Flex>
+                            <Text className='options-label' size='3'>📧 Contact Us</Text>
+                            <Flex direction='column' gap='2'>
+                                <Text size='2'>
+                                    Email us directly at: <Link href="mailto:help@techworks.studio">help@techworks.studio</Link>
+                                </Text>
+                            </Flex>
+                            <Flex direction='column' gap='2'>
+                                <Text size='2'>
+                                    Thank you for trusting TokenXtractor to streamline your workflow. We're here to grow with you!
+                                </Text>
+                            </Flex>
+                        </Flex>
                     </Tabs.Content>
 
                     <Tabs.Content value="about">
-                        <Text size="2">About the plugin, dev info, tips</Text>
+                        <Flex direction='column' gap='3'>
+                            <Text className='options-label' size='3'>About techworks studio</Text>
+                            <Flex direction='column' gap='2'>
+                                <Text size='2'>
+                                    TokenXtractor is brought to you by <Strong>techworks studio</Strong>, a hybrid tech consultancy and R&D studio. At techworks, we create digital product solutions for bold ideas while simultaneously developing in-house tools that seek to innovate and push the boundaries of the way we work.
+                                </Text>
+                                <Text size='2'>
+                                    TokenXtractor is just one of many tools in our mission to shape the future of tech through user-first design and cutting-edge solutions.
+                                </Text>
+                            </Flex>
+                            <Text className='options-label' size='3'>What is TokenXtractor?</Text>
+                            <Flex direction='column' gap='2'>
+                                <Text size='2'>
+                                    TokenXtractor is a lightweight, intuitive plugin designed to help designers and developers streamline their workflows by extracting design tokens directly from your Figma projects and converting them into code-friendly objects and variables.
+                                </Text>
+                            </Flex>
+                            <Text className='options-label' size='3'>Why TokenXtractor Exists?</Text>
+                            <Flex direction='column' gap='2'>
+                                <Text size='2'>
+                                    Designing for scalability and consistency shouldn't be complicated. TokenXtractor empowers individuals and teams to focus on creativity instead of repetitive tasks. Whether you’re designing a simple web page or creating a whole design system, TokenXtractor simplifies the design-development handoff, saving you from the time-consuming task of manually converting local variables to code.
+                                </Text>
+                            </Flex>
+                        </Flex>
                     </Tabs.Content>
                 </Box>
             </ScrollArea>
